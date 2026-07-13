fe77215dd2398044374ec97b2bec6477f7fe7c19
Hanif Isya Annafi <hanifisya.annafi02@gmail.com>
2026-07-13 15:28:47 +0700

    Update fix config vite

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,17 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                'resources/js/landing.js',
+                'resources/js/login.js',
+                'resources/js/register.js',
+                'resources/js/buku.js',
+                'resources/js/anggota.js',
+                'resources/js/pengembalian.js',
+                'resources/js/user_katalog.js',
+            ],
             refresh: true,
         }),
         tailwindcss(),
